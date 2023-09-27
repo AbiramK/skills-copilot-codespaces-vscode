653f4adba15918b439ff03e5bb89383372b4fba4
= <=>
2023-09-27 13:26:59 +0530

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,34 @@
+// create web server with express
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// create a route for the app
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// start the server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// Path: comments.js
+// create web server with express
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// create a route for the app
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// start the server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// Path: comments.js
+// create web server with express
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// create a route for the app
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// start the server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
